f0540c31f3efc88ab5809947a3cdbc669ca3200b
lazaar issam <lazaar.issam@hotmail.com>
2020-02-02 22:37:10 +0100

    Data

--- app/src/main/java/com/example/arduinomobileapp/DataActivity.java
+++ app/src/main/java/com/example/arduinomobileapp/DataActivity.java
@@ -1,14 +1,27 @@
 package com.example.arduinomobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DataActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    String url_data ="http://issamdata.alwaysdata.net/data.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+        //--------------------------All products
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        //------------------------------------------
     }
 }
