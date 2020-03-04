9b7ace46a15e62f65f492dfe2f7081a74999ecbb
lazaar issam <lazaar.issam@hotmail.com>
2020-03-04 17:53:38 +0100

    apidata

--- app/src/main/java/com/example/arduinomobileapp/DataActivity.java
+++ app/src/main/java/com/example/arduinomobileapp/DataActivity.java
@@ -29,7 +29,8 @@ public class DataActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
-    String url_data ="http://issamdata.alwaysdata.net/select.php";
+    String url_data = "http://issamdata.alwaysdata.net/select.php";
+    String url_api_data = "https://api.thingspeak.com/channels/1001656/fields/1.json?api_key=Y8RRGNGOEZ1X9EBP&fbclid=IwAR2OAMaI5z6DXhqw07gDmgHgdvgJMYXhmNauxUZnWwICRzvWC379KstaQBY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +42,8 @@ public class DataActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         //------------------------------------------
-        getData();
+        //getData();
+        getapiData();
         //------------------------------------------
         Log.d(String.valueOf(DataActivity.this),"Tokeeen: "+db.afficherTousUser().get(0).getToken());
     }
@@ -81,6 +83,39 @@ public class DataActivity extends AppCompatActivity {
                 return params;
             }
         };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void getapiData(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url_api_data, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(String.valueOf(DataActivity.this),"responsee: "+response.toString());
+                try {
+                    ArrayList<Information> ListSncf = new ArrayList<Information>();
+                    JSONObject jsonObj = new JSONObject(response);
+                    JSONArray ja_data = jsonObj.getJSONArray("feeds");
+                    for(int i=0;i<ja_data.length();i++){
+                        JSONObject jsonObject = ja_data.getJSONObject(i);
+                        Information in = new Information();
+                        in.setDate(jsonObject.getString("created_at"));
+                        in.setHome(jsonObject.getString("field1"));
+                        ListSncf.add(in);
+                    }
+
+                    adapter = new recyclerAdapter(ListSncf, DataActivity.this);
+                    recyclerView.setAdapter(adapter);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(String.valueOf(DataActivity.this),"error: "+error.getMessage());
+            }
+        });
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
     }
